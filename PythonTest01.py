8c30a6f69b3103c471f6564aa0717c776280d0b5
Royce <yizhi.luo@yale.edu>
2023-10-13 14:08:18 -0400

    test02

--- PythonTest01.py
+++ PythonTest01.py
@@ -4,10 +4,13 @@ import matplotlib.pyplot as plt
 print("Hello")
 print("A change has been made!")
 
+x1 = np.pi
 x = np.linspace(0,10,100)
 y = 1/(5**2-x**2-1j*2*x)
 
 plt.figure()
 plt.plot(x,np.imag(y))
 plt.show()
+
+
 
